c0a75f05e82653ebc45ed71311bd917c8b904dac
mengbixiaojia <945975932@qq.com>
2019-08-19 20:09:35 +0800

    第一次提交

---
+++ HR/EFEntity/MyDBContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Reflection;
+
+namespace EFEntity
+{
+    public class MyDBContext:DbContext
+    {
+        public MyDBContext() :  base("name=sql")
+        {
+            //Database.SetInitializer<MyDBContext>(null);
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Configurations.AddFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        
+        public DbSet<config_file_third_kind> config_file_third_kind { get; set; }
+    }
+}
---
+++ HR/EFEntity/config_file_third_kind.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFEntity
+{
+    public class config_file_third_kind
+    {
+        public int Id { get; set; }
+        public char first_kind_id { get; set; }
+        public string first_kind_name { get; set; }
+        public char  second_kind_id { get; set; }
+        public string second_kind_name { get; set; }
+        public char third_kind_id { get; set; }
+        public string third_kind_name { get; set; }
+        public string third_kind_sale_id { get; set; }
+        public char third_kind_is_retail { get; set; }
+    }
+}
