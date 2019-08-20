52c9c94a10481f36e45ecc436edd8edc231e3d90
c-msr <2727884520@qq.com>
2019-08-20 10:59:48 +0800

    第一次提交

---
+++ HR/IOC/IocCreate.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Practices.Unity.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using System.Configuration;
+
+namespace IOC
+{
+    public class IocCreate
+    {
+        private static UnityContainer GetBLLSeciton()
+        {
+            UnityContainer ioc = new UnityContainer();
+            ExeConfigurationFileMap ecf = new ExeConfigurationFileMap();
+            ecf.ExeConfigFilename = @"D:\作业\net\MVC\HR\UI\Untity.config";
+            Configuration cf = ConfigurationManager.OpenMappedExeConfiguration(ecf, ConfigurationUserLevel.None);
+            UnityConfigurationSection cfs = cf.GetSection("unity") as UnityConfigurationSection;
+            ioc.LoadConfiguration(cfs, "containerOne");
+            return ioc;
+        }
+    }
+}
